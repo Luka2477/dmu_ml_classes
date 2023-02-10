c39c138c8528b69bf66ae38a49ecc3972f9945ba
luka2477 <luke.da.duke@hotmail.com>
2023-02-10 11:21:33 +0100

    done: task 1.2

---
+++ day02_linear_regression/task2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    X = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * X + np.random.randn(100, 1)
+
+    plt.plot(X, y, "b.")
+    plt.axis([0, 2, 0, 15])
+
+    plt.plot()
+    plt.show()
