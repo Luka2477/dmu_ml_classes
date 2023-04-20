a9e23aaa9fa3992a68e039020ea67f4a82f585d3
luka2477 <luke.da.duke@hotmail.com>
2023-04-20 11:57:21 +0200

    done

---
+++ day12_traveling_salesman/found/sol_mike.py
@@ -0,0 +1,136 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+import math
+import sys
+
+data = pd.read_csv('../data/TSPcities1000.txt', sep='\s+', header=None)
+data = pd.DataFrame(data)
+
+x = data[1].to_numpy()
+y = data[2].to_numpy()
+
+
+def createRandomRoute():
+    tour = [[i] for i in range(ROUTE_STOPS)]
+    random.shuffle(tour)
+    return tour
+
+
+# plot the tour - Adjust range 0..len, if you want to plot only a part of the tour.
+def plotCityRoute(route):
+    for i in range(len(route) - 1):
+        plt.plot([x[route[i][0]], x[route[i + 1][0]]], [y[route[i][0]], y[route[i + 1][0]]], 'ro-')
+    # Add the line connecting the last city to the first city to complete the tour
+    plt.plot([x[route[-1][0]], x[route[0][0]]], [y[route[-1][0]], y[route[0][0]]], 'ro-')
+    plt.show()
+
+
+def generate_tours(size):
+    tours = []
+    for _ in range(size):
+        individual = createRandomRoute()
+        tours.append(individual)
+    return tours
+
+
+def distance_of_tour(tour):
+    distance = 0
+    for i in range(len(tour) - 1):
+        distance += distancebetweenCities(tour[i], tour[i + 1])
+    # Add distance from last city back to the first city
+    distance += distancebetweenCities(tour[-1], tour[0])
+    return distance
+
+
+def distancebetweenCities(city1, city2):
+    xDistance = abs(x[city1] - (x[city2]))
+    yDistance = abs(y[city1] - (y[city2]))
+    distance = math.sqrt((xDistance * xDistance) + (yDistance * yDistance))
+    return distance
+
+
+def select_parents(tours):
+    parent1 = random.choice(tours)
+    parent2 = random.choice(tours)
+    if distance_of_tour(parent1) <= distance_of_tour(parent2):
+        return parent1
+    else:
+        return parent2
+
+
+def crossover(parent1, parent2):
+    return ordered_crossover(parent1, parent2)
+
+
+def ordered_crossover(parent1, parent2):
+    size = len(parent1)
+
+    start, end = sorted(random.sample(range(size), 2))
+
+    child = [None] * size
+    child[start:end] = parent1[start:end]
+
+    pointer = end
+    for i in range(size):
+        parent2_pointer = (i + end) % size
+        if parent2[parent2_pointer] not in child:
+            child[pointer] = parent2[parent2_pointer]
+            pointer = (pointer + 1) % size
+    return child
+
+
+def mutate(individual, mutation_rate):
+    if random.random() < mutation_rate:
+        i, j = random.sample(range(len(individual)), 2)
+
+        if i > j:
+            i, j = j, i
+
+        individual[i:j] = individual[i:j][::-1]
+
+    return individual
+
+
+def genetic_algorithm(population_size, generations):
+    tours = generate_tours(population_size)
+    best_individual = None
+    best_fitness = sys.maxsize
+    for generation in range(generations):
+        new_population = []
+        for i in range(population_size):
+            parent1 = select_parents(tours)
+            parent2 = select_parents(tours)
+            child = crossover(parent1, parent2)
+            child = mutate(child, MUTATION_RATE)
+            new_population.append(child)
+
+        tours = new_population
+
+        curr_best = min(tours, key=distance_of_tour)
+        curr_fitness = distance_of_tour(curr_best)
+        if curr_fitness < best_fitness:
+            best_individual = curr_best
+            best_fitness = curr_fitness
+
+        if generation % 2 == 0:
+            print(f"Generation {generation}:"
+                  #   f" Best solution: {best_individual},"
+                  f" Fitness: {best_fitness}")
+
+    return best_individual, best_fitness
+
+
+# fake_tour = [[1], [2], [6], [50]]
+# print(distance_of_tour(fake_tour))
+# plotCityRoute(fake_tour)
+
+# Here comes your GA program...
+POP_SIZE = 1000
+MUTATION_RATE = 0.05
+ROUTE_STOPS = 15
+
+best_solution, best_fitness = genetic_algorithm(population_size=POP_SIZE, generations=50)
+
+print(f"\nBest Solution: {best_solution}\nBest Fitness: {best_fitness}")
+plotCityRoute(best_solution)
---
+++ day12_traveling_salesman/given/TSP.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+import math
+
+
+def create_random_route():
+    tour = [[i] for i in range(1000)]
+    random.shuffle(tour)
+    return tour
+
+
+# plot the tour - Adjust range 0..len, if you want to plot only a part of the tour.
+def plot_city_route(route):
+    for i in range(0, len(route)):
+        plt.plot(x[i:i + 2], y[i:i + 2], 'ro-')
+    plt.show()
+
+
+# calculate distance between cities
+def distance_between_cities(city1x, city1y, city2x, city2y):
+    xDistance = abs(city1x - city2x)
+    yDistance = abs(city1y - city2y)
+    distance = math.sqrt((xDistance * xDistance) + (yDistance * yDistance))
+    return distance
+
+
+data = pd.read_csv('TSPcities1000.txt', sep='\s+', header=None)
+data = pd.DataFrame(data)
+
+x = data[1]
+y = data[2]
+plt.plot(x, y, 'r.')
+plt.show()
+
+
+# Alternativ kode:
+#  for i in range(0, len(route)-1:
+#     plt.plot([x[route[i]],x[route[i+1]]], [y[route[i]],y[route[i+1]]], 'ro-')
+
+tour = create_random_route()
+print(tour)
+plot_city_route(tour)
+
+# distance between city number 100 and city number 105
+dist = distance_between_cities(x[100], y[100], x[105], y[105])
+print('Distance, % target: ', dist)
+
+best_score_progress = []  # Tracks progress
+
+# replace with your own calculations
+fitness_gen0 = 1000  # replace with your value
+print('Starting best score, % target: ', fitness_gen0)
+
+best_score = fitness_gen0
+# Add starting best score to progress tracker
+best_score_progress.append(best_score)
+
+# Here comes your GA program...
+best_score = 980
+best_score_progress.append(best_score)
+best_score = 960
+best_score_progress.append(best_score)
+
+# GA has completed required generation
+print('End best score, % target: ', best_score)
+
+plt.plot(best_score_progress)
+plt.xlabel('Generation')
+plt.ylabel('Best Fitness - route length - in Generation')
+plt.show()
